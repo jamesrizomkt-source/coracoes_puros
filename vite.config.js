db005c90489da7d512cea744e6d6e4cdff8df586
James Rizo <jamesrizo@MacBook-Air-de-James.local>
2026-07-06 16:38:57 -0300

    Remover painel admin-novo redundante

--- vite.config.js
+++ vite.config.js
@@ -10,8 +10,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        admin: resolve(__dirname, 'admin.html'),
-        admin_novo: resolve(__dirname, 'admin-novo.html')
+        admin: resolve(__dirname, 'admin.html')
       }
     }
   }
